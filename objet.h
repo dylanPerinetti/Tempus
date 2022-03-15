d2a4a41e2ea6bef505a490596cae9989c6546210
dylanPerinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 10:55:00 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -1,19 +1,25 @@
-#ifndef __LIB__
-#define __LIB__
-
-#include <stdio.h>
-
-#endif
-
 #ifndef __OBJET_H__
 #define __OBJET_H__
 /*-------------------- Pions ------------------------*/
 
 typedef struct Pions
-{
-        int Joueur.couleur;
-        int Coordx;
-        int Coordy;
+{       
+        uint8_t id_pion;// uint8_t sigifie un nb entier non signer sur 1 Octer contre 4 Octer pour un simple int 
+        int couleur;//Car la couleur est un Héritage du joueur 
+        int Coordx;//tableux ?
+        int Coordy;//tableux ?
 }Pions;
+
+
+/*-------------------- Ville ------------------------*/
+
+typedef struct Ville
+{
+        uint8_t id_ville;//identification de la piece
+        uint8_t couleur;//Car la couleur est un Héritage du joueur 
+        int Coordx;//tableux ?
+        int Coordy;//tableux ?
+        uint8_t niveaux_ville;
+}Ville;
 
 #endif
