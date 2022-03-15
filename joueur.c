3c76441e49c5f27ad3ef8d606f134e2a847671de
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-16 00:31:14 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -18,12 +18,12 @@ void CreeJoueurs(Joueur* j,uint8_t c)	//pour la crééation d'un nouveaux joueur
 	for (uint8_t i = 0; i < 16; ++i)
 	{
 		Pions pion_i={i,c,-1,-1};
-		j->pions_possede[i]=pion_i;
+		j->pion_possede[i]=pion_i;
 	}
 	for (uint8_t i = 0; i < 8; ++i)
 	{
-		Ville ville_i={i,c,-1,-1,0};
-		j->ville_possede[i]=ville_i;
+		Cite cite_i={i,c,-1,-1,0};
+		j->cite_possede[i]=cite_i;
 	}
 }
 void Cree4Joueurs(Joueur tab[])
@@ -31,11 +31,36 @@ void Cree4Joueurs(Joueur tab[])
 	for (uint8_t i = 0; i < 4; ++i)
 		CreeJoueurs(&tab[i],i+1);
 }
-void AugmenterNiveaux(Joueur* j)
+void AugmenterNiveauxJoueur(Joueur* j)
 {
 	++j->niveaux_joueur;
 }
-void AfficherNiveaux(Joueur* j)
+void AfficherNiveauxJoueur(Joueur* j)
 {
 	printf("\nLe NV du Joueur n°%u est de %u \n", j->couleur, j->niveaux_joueur);
+}
+
+int InventairePions(Joueur* j)
+{
+	int nb_de_pion_dans_le_stuff = 0;
+
+	for (uint8_t i = 0; i < 16; ++i)
+	{
+		if(j->pion_possede[i].Coordx == -1 && j->pion_possede[i].Coordy == -1)
+			++nb_de_pion_dans_le_stuff;
+	}
+
+	return nb_de_pion_dans_le_stuff;
+}
+int InventaireCite(Joueur* j)
+{
+	int nb_de_cite_dans_le_stuff=0;
+
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		if(j->cite_possede[i].Coordx == -1 && j->cite_possede[i].Coordy == -1)
+			++nb_de_cite_dans_le_stuff;
+	}
+
+	return nb_de_cite_dans_le_stuff;
 }
