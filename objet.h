1beb0ea25f419e1297ae6e1f26748539d4b6fdc8
dylanPerinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 11:32:19 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -4,10 +4,10 @@
 
 typedef struct Pions
 {       
-        uint8_t id_pion;// uint8_t sigifie un nb entier non signer sur 1 Octer contre 4 Octer pour un simple int 
-        uint8_t couleur;//Car la couleur est un Héritage du joueur 
-        int Coordx;//tableux ?
-        int Coordy;//tableux ?
+        uint8_t id_pion;        // uint8_t sigifie un nb entier non signer sur 1 Octer contre 4 Octer pour un simple int 
+        uint8_t couleur;        //Car la couleur est un Héritage du joueur 
+        int Coordx;             //tableux ?
+        int Coordy;             //tableux ?
 }Pions;
 
 
@@ -15,10 +15,10 @@ typedef struct Pions
 
 typedef struct Ville
 {
-        uint8_t id_ville;//identification de la piece
-        uint8_t couleur;//Car la couleur est un Héritage du joueur 
-        int Coordx;//tableux ?
-        int Coordy;//tableux ?
+        uint8_t id_ville;       //identification de la piece
+        uint8_t couleur;        //Car la couleur est un Héritage du joueur 
+        int Coordx;             //tableux ?
+        int Coordy;             //tableux ?
         uint8_t niveaux_ville;
 }Ville;
 
