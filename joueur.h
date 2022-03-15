84064479bddc810c44bacd0d3c4481d5b8c25c10
dylanPerinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 08:39:26 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -1,5 +1,15 @@
 #ifndef __JOUEUR_H__
 #define __JOUEUR_H__
-typedef struct Joueur Joueur;
+#include <stdio.h>
+/*-------------------- Joueur ------------------------*/
+
+typedef struct Joueur
+{
+	signed char nom_utilisateur[255];
+	int couleur;
+	int niveaux_joueur;
+//	Pions pions_possede[16];
+//	Cite cite_possede[8];
+}Joueur;
 typedef struct Pions Pions;
 #endif
