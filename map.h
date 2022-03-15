0af0376cb577c3fdfabfe52d920a4405a6697cbc
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 21:34:26 +0100

    Create map.h

---
+++ map.h
@@ -0,0 +1,10 @@
+#ifndef __MAP__
+#define __MAP__
+typedef struct CarteTerrain
+{
+  //uint8_t type_terrain;
+  //Salut MrTNTX peut tu me mettre au courrant quand tu vois ce messsage pour 
+  //que l'on se mette d'accord sur la structure de terrain et surtout d'une uniter de surface
+  //FAIT LE MAR 15 MARS 21:34
+}CarteTerrain;
+#endif
