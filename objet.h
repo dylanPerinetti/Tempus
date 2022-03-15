6fd5e3df40f1966aaab2f42743479e90d1de833b
dylanPerinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 08:46:24 +0100

    Create objet.h

---
+++ objet.h
@@ -0,0 +1,19 @@
+#ifndef __LIB__
+#define __LIB__
+
+#include <stdio.h>
+
+#endif
+
+#ifndef __OBJET_H__
+#define __OBJET_H__
+/*-------------------- Pions ------------------------*/
+
+typedef struct Pions
+{
+        int Joueur.couleur;
+        int Coordx;
+        int Coordy;
+}Pions;
+
+#endif
