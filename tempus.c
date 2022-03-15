12801770aacd1f4c2e1413cdd32192b15dcb97a5
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 22:46:24 +0100

    Update tempus.c

--- tempus.c
+++ tempus.c
@@ -3,31 +3,35 @@
 #include "objet.h"
 #include "joueur.h"
 
-#define NETTOYER_TERMINAL "clear"		//Si vous etes sur windows dans le CMD remplacer par "cls"  
+#define NETTOYER_TERMINAL "clear"				//Si vous etes sur windows dans le CMD remplacer par "cls"  
 
 int main()
 {
-	system(NETTOYER_TERMINAL);			//shell Netoyer le terminal 
 	int choix_utilisateur=0;
 	Joueur tableau_joueur[4];
+
+
+	system(NETTOYER_TERMINAL);					//shell Netoyer le terminal 
 	printf("\nBonjour, Bienvenue a sur TEMPUS 3.0\n\n\n");
+	
+
 	do{
+	
 		printf("\nMENU:\n\n1. JOUER \n2. REGLES DU JEU \n3. QUITTER \n") ;
-		scanf("%d",&choix_utilisateur);
+		scanf("%d",&choix_utilisateur);			//Améliorer la Secu avec fgets ou JCP ...
+	
+	
 		switch(choix_utilisateur)
     	{
 	        case 1:
 	            system(NETTOYER_TERMINAL);
-	            CreeJoueurs(&tableau_joueur[0],1);
-	            CreeJoueurs(&tableau_joueur[1],2);
-	            CreeJoueurs(&tableau_joueur[2],3);
-	            CreeJoueurs(&tableau_joueur[3],4);
-	            //DebutJeu();			//Initialisations du jeu
+	            Cree4Joueurs(tableau_joueur);
+	            //DebutJeu();					//Initialisations du jeu
 	            //AfficherPlateau();
 	            break;
 	        case 2:
 	            system(NETTOYER_TERMINAL);
-	            //AfficherReglesDuJeu();//Affiche les regles du jeu 
+	            //AfficherReglesDuJeu();		//Affiche les regles du jeu 
 	            break;
 	        case 3:
 	            system(NETTOYER_TERMINAL);
@@ -39,6 +43,15 @@ int main()
 	            return 0 ;
 	            break;
 	    }
+	    AugmenterNiveaux(&tableau_joueur[3]);	//TESTE 
+		AfficherNiveaux(&tableau_joueur[1]);	//TESTE
+		AugmenterNiveaux(&tableau_joueur[1]);	//TESTE 
+		AfficherNiveaux(&tableau_joueur[3]);	//TESTE
+	    AugmenterNiveaux(&tableau_joueur[3]);	//TESTE 
+		AfficherNiveaux(&tableau_joueur[1]);	//TESTE
+		AugmenterNiveaux(&tableau_joueur[1]);	//TESTE 
+		AfficherNiveaux(&tableau_joueur[1]);	//TESTE
+
     } while (choix_utilisateur!=4);
     return 0;
 }
