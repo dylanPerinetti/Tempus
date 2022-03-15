c8aa136ff981f38ee375fa9092dc126642cba238
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 22:46:53 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -5,13 +5,17 @@
 
 typedef struct Joueur
 {
-	char nom_utilisateur[255];
+	//char nom_utilisateur[255];
 	int couleur;
 	int niveaux_joueur;
 	Pions pions_possede[15];
 	Ville ville_possede[7];
 }Joueur;
 
+
 void CreeJoueurs(Joueur* j,uint8_t c);
+void Cree4Joueurs(Joueur tab[]);
+void AugmenterNiveaux(Joueur* j);
+void AfficherNiveaux(Joueur* j);
 
 #endif
