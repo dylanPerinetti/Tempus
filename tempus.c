8d8520268ab99ffa9a4f4630af2b4c27f373e938
dylanPerinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-15 10:57:00 +0100

    Update tempus.c

--- tempus.c
+++ tempus.c
@@ -1,9 +1,15 @@
-#import <stdlib.h>
+/*-------------------- Biblio ------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "objet.h"
 #include "joueur.h"
+
+/*----------------------------------------------------*/
 
 int main()
 {
 	int choix_utilisateur=0;
+	Joueur joueur1 , joueur2 , joueur3 , joueur4;
 
 	do{
 		printf("\nBienvenue \n1. JOUER \n2. REGLES DU JEU \n3. QUITTER \n") ;
@@ -12,12 +18,16 @@ int main()
     	{
 	        case 1:
 	            system("cls"); //Windows
-	            //debut_jeu();
-	            //affichagePlateau();
+	            CreeJoueurs(&joueur1,1);
+	            CreeJoueurs(&joueur2,2);
+	            CreeJoueurs(&joueur3,3);
+	            CreeJoueurs(&joueur4,4);
+	            //DebutJeu();
+	            //AfficherPlateau();
 	            break;
 	        case 2:
 	            system("cls"); //Windows
-	            //regles_du_jeu();
+	            //AfficherReglesDuJeu();
 	            break;
 	        case 3:
 	            system("cls"); //Windows
@@ -31,12 +41,12 @@ int main()
 	    }
 	    
 
-	    /*________________________TEST____________________________
-	    Joueur j1;//créé un joueur
-	    scanf("%s",j1.nom_utilisateur);//changer son nom
-	    j1.niveaux_joueur=300;
-	    printf("Le nom du joueur est :%s,\net son lvl est : %d",j1.nom_utilisateur,j1.niveaux_joueur);//afficher les changement
-	    ________________________TEST____________________________*/
+	    /*________________________TEST____________________________*/
+	    Joueur joueur1;//créé un joueur
+	    scanf("%s",joueur1.nom_utilisateur);//changer son nom
+	    joueur1.niveaux_joueur=300;
+	    printf("Le nom du joueur est :%s,\net son lvl est : %d",joueur1.nom_utilisateur,joueur1.niveaux_joueur);//afficher les changements
+	    /*________________________TEST____________________________*/
     
 
     } while (choix_utilisateur!=4);
