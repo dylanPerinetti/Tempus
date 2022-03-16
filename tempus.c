4447716d30d99f7e7da8dbf4838dcadedd176d47
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-16 20:11:50 +0100

    Update tempus.c

--- tempus.c
+++ tempus.c
@@ -6,7 +6,7 @@
 
 #define NETTOYER_TERMINAL "clear"				//Si vous etes sur windows dans le CMD remplacer par "cls"  
 
-int main()
+int main(void)
 {
 	int choix_utilisateur=0;
 	Joueur tableau_joueur[4];
@@ -47,23 +47,24 @@ int main()
 	    //______________________   ZONE TESTE   ________________________//
 
 
-	    /*
-	    DeplacerPionDroit(&tableau_joueur[2].pion_possede[7]);       //Déplacer pion a Droite
+	    
+		DeplacerPionDroite(&tableau_joueur[2].pion_possede[7]);       //Déplacer pion a Droite
 		DeplacerPionGauche(&tableau_joueur[2].pion_possede[7]);      //Déplacer pion a Gauche
-		DeplacerPionHautDroit(&tableau_joueur[2].pion_possede[7]);   //Déplacer pion en Haut a Droite
+		DeplacerPionHautDroite(&tableau_joueur[2].pion_possede[7]);   //Déplacer pion en Haut a Droite
 		DeplacerPionHautGauche(&tableau_joueur[2].pion_possede[7]);  //Déplacer pion en Haut a Gauche
-		DeplacerPionBasDroit(&tableau_joueur[2].pion_possede[7]);    //Déplacer pion en Bas a Droite
+		DeplacerPionBasDroite(&tableau_joueur[2].pion_possede[7]);    //Déplacer pion en Bas a Droite
 		DeplacerPionBasGauche(&tableau_joueur[2].pion_possede[7]);   //Déplacer pion en Bas a Gauche
-	    AfficherIDCite(tableau_joueur[2].cite_possede[7]);
-	    printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
-	    printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
-	    AugmenterNiveauxJoueur(&tableau_joueur[3]);				//TESTE 
+		AfficherIDCite(tableau_joueur[2].cite_possede[7]);
+		printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
+		printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
+	   	AugmenterNiveauxJoueur(&tableau_joueur[3]);				//TESTE 
 		AfficherNiveauxJoueur(&tableau_joueur[1]);				//TESTE
 		AfficherInventaireJoueur(&tableau_joueur[1]);			//TESTE
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
 		ChangerCouleurCite(tableau_joueur[1],&tableau_joueur[2].cite_possede[2]);//TESTE
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
-		*/
+		ChangerCouleurCite(tableau_joueur[3],&tableau_joueur[2].cite_possede[2]);//TESTE
+		
 
 		//_____________________   FIN ZONE TESTE   _______________________//
     }while (choix_utilisateur!=4);
