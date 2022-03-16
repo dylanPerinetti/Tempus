962ee85191daf4eac5f52df9ff9a5595d118752d
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-16 10:59:08 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -10,8 +10,10 @@
 //		FONCTION		//
 /////////////////////////
 
-void CreeJoueurs(Joueur* j,uint8_t c)	//pour la crééation d'un nouveaux joueur
+void CreeJoueur(Joueur* j,uint8_t c)	//pour la crééation d'un nouveaux joueur
 {
+	//malloc(sizeof(char)*);
+	//free();
 	//strcpy(j->nom_utilisateur,nom);
 	j->couleur=c;
 	j->niveaux_joueur=0;
@@ -26,21 +28,33 @@ void CreeJoueurs(Joueur* j,uint8_t c)	//pour la crééation d'un nouveaux joueur
 		j->cite_possede[i]=cite_i;
 	}
 }
-void Cree4Joueurs(Joueur tab[])
+void Cree4Joueurs(Joueur tab[])			//Pour Créé 4 Joueur
 {
 	for (uint8_t i = 0; i < 4; ++i)
-		CreeJoueurs(&tab[i],i+1);
+		CreeJoueur(&tab[i],i+1);
 }
-void AugmenterNiveauxJoueur(Joueur* j)
+void AugmenterNiveauxJoueur(Joueur* j)	//Pour Indenter le niveux d'un joueuer 
 {
 	++j->niveaux_joueur;
 }
-void AfficherNiveauxJoueur(Joueur* j)
+void AfficherNiveauxJoueur(Joueur* j)	//Pour Afficher le niveaux d'un joueur
 {
 	printf("\nLe NV du Joueur n°%u est de %u \n", j->couleur, j->niveaux_joueur);
 }
+void AfficherInventaireJoueur(Joueur* j)//Pour Afficher L'inventaire d'un joueur
+{
+	int *tab=InventaireJoueur(j);
+	printf("Le joueur n°%d Possède %d Pions et Possède %d Cite",j->couleur ,tab[0],tab[1]);
+}
 
-int InventairePions(Joueur* j)
+int* InventaireJoueur(Joueur* j)		//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
+{
+	static int tab[2];
+	tab[0] = InventairePions(j);
+	tab[1] = InventaireCite(j);
+	return tab;
+}
+int InventairePions(Joueur* j)			//Pour Répertorier les Pions d'un joueur dans l'inventaire
 {
 	int nb_de_pion_dans_le_stuff = 0;
 
@@ -52,7 +66,7 @@ int InventairePions(Joueur* j)
 
 	return nb_de_pion_dans_le_stuff;
 }
-int InventaireCite(Joueur* j)
+int InventaireCite(Joueur* j)			//Pour Répertorier les Cite d'un joueur dans l'inventaire
 {
 	int nb_de_cite_dans_le_stuff=0;
 
