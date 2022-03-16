c35326e3aab10d4a17c503a3061b3e8073d5a356
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-16 12:20:19 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -10,19 +10,18 @@ typedef struct Joueur
 	uint8_t niveaux_joueur;
 	Pions pion_possede[16];
 	Cite cite_possede[8];
-}Joueur;
+}Joueur;					//Pour Cree la Structure d'un joueur
 
+void CreeJoueur(Joueur* j,uint8_t c);		//Pour la crééation d'un nouveaux joueur
+void Cree4Joueurs(Joueur tab[]);		//Pour Créé 4 Joueur
+void AugmenterNiveauxJoueur(Joueur* j);		//Pour Indenter le niveux d'un joueuer 
 
-void CreeJoueur(Joueur* j,uint8_t c);
-void Cree4Joueurs(Joueur tab[]);
-void AugmenterNiveauxJoueur(Joueur* j);
+void AfficherNiveauxJoueur(Joueur* j);		//Pour Afficher le niveaux d'un joueur
+void AfficherInventaireJoueur(Joueur* j);	//Pour Afficher L'inventaire d'un joueur
 
-void AfficherNiveauxJoueur(Joueur* j);
-void AfficherInventaireJoueur(Joueur* j);
-
-int* InventaireJoueur(Joueur* j);
-int InventairePions(Joueur* j);
-int InventaireCite(Joueur* j);
+int* InventaireJoueur(Joueur* j);		//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
+int InventairePions(Joueur* j);			//Pour Répertorier les Pions d'un joueur dans l'inventaire
+int InventaireCite(Joueur* j);			//Pour Répertorier les Cite d'un joueur dans l'inventaire
 
 
 #endif
