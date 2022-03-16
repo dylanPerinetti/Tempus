647e6ec00c4ae678eaec880d0bf76e8d9584faea
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-16 17:46:15 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -1,25 +1,22 @@
 #ifndef __OBJET_H__
 #define __OBJET_H__
-/*-------------------- Pions ------------------------*/
-
+/*-------------------- OBJET ------------------------*/
 typedef struct Pions
 {       
-        uint8_t id_pion;        // uint8_t sigifie un nb entier non signer sur 1 Octer contre 4 Octer pour un simple int 
-        uint8_t couleur;        //Car la couleur est un Héritage du joueur 
-        int Coordx;             //tableux ?
-        int Coordy;             //tableux ?
-}Pions;
-
-
-/*-------------------- CITE -------------------------*/
-
+        uint8_t id_pion;                        //uint8_t sigifie un nb entier non signer sur 1 Octer contre 4 Octer pour un simple int 
+        uint8_t couleur;                        //Car la couleur est un Héritage du joueur 
+        int coord_x;
+        int coord_y;
+}Pions;                                         //Cree la structure d'un pions
 typedef struct Cite
 {
-        uint8_t id_cite;       //identification de la piece
-        uint8_t couleur;        //Car la couleur est un Héritage du joueur 
-        int Coordx;             //tableux ?
-        int Coordy;             //tableux ?
+        uint8_t id_cite;                        //identification de la piece
+        uint8_t couleur;                        //Car la couleur est un Héritage du joueur 
+        int coord_x;                            //tableux ?
+        int coord_y;                            //tableux ?
         uint8_t niveaux_cite;
-}Cite;
+}Cite;                                          //Cree la structure d'une Cite
+
+void AfficherCouleurCite(Cite c_);
 
 #endif
