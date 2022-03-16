e9e3f1e13047f7f7fb6fa5898ad6f4efd7777c19
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-17 00:18:25 +0100

    Update tempus.c

--- tempus.c
+++ tempus.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "objet.h"
 #include "joueur.h"
-#include "map.h"
+//#include "map.h"
 
 #define NETTOYER_TERMINAL "clear"				//Si vous etes sur windows dans le CMD remplacer par "cls"  
 
@@ -20,7 +20,6 @@ int main(void)
 	
 		printf("\nMENU:\n\n1. JOUER \n2. REGLES DU JEU \n3. QUITTER \n") ;
 		scanf("%1d",&choix_utilisateur);			//Améliorer la Secu avec fgets ou JCP ...
-	
 	
 		switch(choix_utilisateur)
     	{
@@ -46,8 +45,6 @@ int main(void)
 
 	    //______________________   ZONE TESTE   ________________________//
 
-
-	    
 		DeplacerPionDroite(&tableau_joueur[2].pion_possede[7]);       //Déplacer pion a Droite
 		DeplacerPionGauche(&tableau_joueur[2].pion_possede[7]);      //Déplacer pion a Gauche
 		DeplacerPionHautDroite(&tableau_joueur[2].pion_possede[7]);   //Déplacer pion en Haut a Droite
@@ -57,7 +54,25 @@ int main(void)
 		AfficherIDCite(tableau_joueur[2].cite_possede[7]);
 		printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
 		printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
-	   	AugmenterNiveauxJoueur(&tableau_joueur[3]);				//TESTE 
+	   	AugmenterNiveauxJoueur(&tableau_joueur[1]);				//TESTE 
+		AfficherNiveauxJoueur(&tableau_joueur[1]);				//TESTE
+		AfficherInventaireJoueur(&tableau_joueur[1]);			//TESTE
+		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
+		ChangerCouleurCite(tableau_joueur[1],&tableau_joueur[2].cite_possede[2]);//TESTE
+		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
+		ChangerCouleurCite(tableau_joueur[3],&tableau_joueur[2].cite_possede[2]);//TESTE
+	    AfficherCoordonneesCite(tableau_joueur[2].cite_possede[7]);
+	    DeplacerPionDroite(&tableau_joueur[2].pion_possede[7]);     //Déplacer pion a Droite
+		DeplacerPionGauche(&tableau_joueur[2].pion_possede[7]);     //Déplacer pion a Gauche
+		DeplacerPionHautDroite(&tableau_joueur[2].pion_possede[7]); //Déplacer pion en Haut a Droite
+		DeplacerPionHautGauche(&tableau_joueur[2].pion_possede[7]); //Déplacer pion en Haut a Gauche
+		DeplacerPionBasDroite(&tableau_joueur[2].pion_possede[7]);	//Déplacer pion en Bas a Droite
+		DeplacerPionBasGauche(&tableau_joueur[2].pion_possede[7]);  //Déplacer pion en Bas a Gauche
+	    AfficherIDCite(tableau_joueur[2].cite_possede[7]);
+	    AfficherIDPion(tableau_joueur[2].pion_possede[7]);
+	    printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
+	    printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
+	    AugmenterNiveauxJoueur(&tableau_joueur[3]);				//TESTE 
 		AfficherNiveauxJoueur(&tableau_joueur[1]);				//TESTE
 		AfficherInventaireJoueur(&tableau_joueur[1]);			//TESTE
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
@@ -65,7 +80,6 @@ int main(void)
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
 		ChangerCouleurCite(tableau_joueur[3],&tableau_joueur[2].cite_possede[2]);//TESTE
 		
-
 		//_____________________   FIN ZONE TESTE   _______________________//
     }while (choix_utilisateur!=4);
     return 0;
