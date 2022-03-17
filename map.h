248e18c58c6e8bc728ffb8dd0a7d570eca23933e
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-17 07:44:14 +0100

    Update map.h

--- map.h
+++ map.h
@@ -9,17 +9,6 @@
 
 int Aleatoir();
 void Erreur(int error);
-void ImageAleatoir(SDL_Surface* image)
-{
-	int terrain = Aleatoir();                         //Donne une valeur random entre 0 et 4 a terrain
-	switch(terrain)
-		{
-			case 0 : *image = SDL_LoadBMP("src/Case_Champ_Dim.bmp"); break;          
-			case 1 : *image = SDL_LoadBMP("src/Case_Foret_Dim.bmp"); break;
-			case 2 : *image = SDL_LoadBMP("src/Case_Prairie_Dim.bmp"); break;       //En fonction de la valeur de terrain,
-			case 3 : *image = SDL_LoadBMP("src/Case_Montagne_Dim.bmp"); break;      //donne un dossier bmp différent a image
-			case 4 : *image = SDL_LoadBMP("src/Case_Colline_Dim.bmp"); break; 
-		}
-}
+void ImageAleatoir(SDL_Surface* image);// PAS SUR QUE LA FONCTION FONCTIONNE !!!
 
 #endif
