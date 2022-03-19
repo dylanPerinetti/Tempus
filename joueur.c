9d92fa849bf485b652d9a6a131d92d955a347b9b
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-19 15:17:05 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -1,3 +1,21 @@
+/*
+
+-------------- TEMPUS --------------
+
+Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus" 
+
+Ce fichier contient les fonction liez au Joueur dans le Jeu Tempus.
+En savoir plus sur leur utilisation dans le ficheier joueur.h
+
+Fait par dylan le 14/03/2022
+Dernière modifications par dylanPerinetti le 19/03/2022
+
+Je suis acctuellement en train de develloper les fonction:
+	- RecupererPion()
+	- 
+
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include "objet.h"
@@ -36,6 +54,8 @@ void Cree4Joueurs(Joueur tab[])			//Pour Créé 4 Joueur
 		CreeJoueur(&tab[i],i+1);
 	}
 }
+
+
 void InitialiserCouleurJoueur(Joueur* _joueur,unsigned char _couleur)
 {
 	_joueur->couleur=_couleur;
@@ -47,14 +67,19 @@ void InitialiserNiveauJoueur(Joueur* _joueur)
 	_joueur->niveaux_joueur=0;
 }
 
+
 void AugmenterNiveauJoueur(Joueur* j)	//Pour Indenter le niveux d'un joueuer 
 {
 	j->niveaux_joueur++;
 }
+
+
 void AfficherNiveauJoueur(Joueur* j)	//Pour Afficher le niveaux d'un joueur
 {
 	printf("\nLe niveau du Joueur%u est %u \n", j->couleur, j->niveaux_joueur);
 }
+
+
 void AfficherInventaireJoueur(Joueur* j)//Pour Afficher L'inventaire d'un joueur
 {
 	int *tab=InventaireJoueur(j);
@@ -62,34 +87,45 @@ void AfficherInventaireJoueur(Joueur* j)//Pour Afficher L'inventaire d'un joueur
 }
 
 
-int* InventaireJoueur(Joueur* j)		//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
+int* InventaireJoueur(Joueur* _joueur)	//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
 {
 	static int tab[2];
 	tab[0] = InventairePions(j);
 	tab[1] = InventaireCite(j);
 	return tab;
 }
-int InventairePions(Joueur* j)			//Pour Répertorier les Pions d'un joueur dans l'inventaire
+
+
+int InventairePions(Joueur* _joueur)	//Pour Répertorier les Pions d'un joueur dans l'inventaire
 {
 	int nb_de_pion_dans_le_stuff = 0;
 
 	for (unsigned char i = 0; i < 16; ++i)
 	{
-		if(j->pion_possede[i].coord_x == -1 && j->pion_possede[i].coord_y == -1)
+		if(_joueur->pion_possede[i].coord_x == -1 && _joueur->pion_possede[i].coord_y == -1)
 			nb_de_pion_dans_le_stuff++;
 	}
 
 	return nb_de_pion_dans_le_stuff;
 }
-int InventaireCite(Joueur* j)			//Pour Répertorier les Cite d'un joueur dans l'inventaire
+
+
+int InventaireCite(Joueur* _joueur)		//Pour Répertorier les Cite d'un joueur dans l'inventaire
 {
 	int nb_de_cite_dans_le_stuff=0;
 
 	for (unsigned char i = 0; i < 8; ++i)
 	{
-		if(j->cite_possede[i].coord_x == -1 && j->cite_possede[i].coord_y == -1)
+		if(_joueur->cite_possede[i].coord_x == -1 && _joueur->cite_possede[i].coord_y == -1)
 			nb_de_cite_dans_le_stuff++;
 	}
 
 	return nb_de_cite_dans_le_stuff;
+}
+
+int RecupererPion(Joueur* _joueur,Pions* _pion)
+{
+	_pion->coord_y=-1;
+	_pion->coord_x=-1;
+	return 0;
 }
