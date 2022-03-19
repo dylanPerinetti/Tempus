2b38e887aecc9ae2b2c89d47aa9d6e4f1bba15ea
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-19 15:21:23 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -1,3 +1,20 @@
+/*
+
+-------------- TEMPUS --------------
+
+Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus" 
+
+
+Ce fichier contient les prototype des fonction lier au joueur dans le jeu Tempus.
+Vous retrouverer par aillieur leur mode d'utilisation .
+Pour mieux comprendre leur fonctionnement rendez-vous sur le fichier "joueur.c" .
+
+
+Fait par dylan le 14/03/2022
+Dernière modifications par dylanPerinetti le 19/03/2022
+*/
+
+
 #ifndef __JOUEUR_H__
 #define __JOUEUR_H__
 #include <stdio.h>
@@ -22,11 +39,11 @@ void AugmenterNiveauJoueur(Joueur* j);			//Pour Indenter le niveux d'un joueuer
 void AfficherNiveauJoueur(Joueur* j);			//Pour Afficher le niveaux d'un joueur
 void AfficherInventaireJoueur(Joueur* j);		//Pour Afficher L'inventaire d'un joueur
 
-int* InventaireJoueur(Joueur* j);				//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
-int InventairePions(Joueur* j);					//Pour Répertorier les Pions d'un joueur dans l'inventaire
-int InventaireCite(Joueur* j);					//Pour Répertorier les Cite d'un joueur dans l'inventaire
+int* InventaireJoueur(Joueur* _joueur);				//Pour Ranger l'inventaire dans un tableau[<PIONS>,<CITE>]
+int InventairePions(Joueur* _joueur);				//Pour Répertorier les Pions d'un joueur dans l'inventaire
+int InventaireCite(Joueur* _joueur);				//Pour Répertorier les Cite d'un joueur dans l'inventaire
 
-void RecupererPion(Joueur* j,Pions* p);			//Placer un pion dans le stuff du joueur
+int RecupererPion(Joueur* _joueur,Pions* _pion);	//Placer un pion dans le stuff du joueur
 
 
 #endif
