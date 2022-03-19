813ca8483d9d51eb6a6cbf3e6b6ae91b631e65d2
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-19 15:19:16 +0100

    Update tempus.c

--- tempus.c
+++ tempus.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+
+-------------- TEMPUS --------------
+
+Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus" 
+
+Ce fichier contient le code Pincipal du jeu (main).
+
+Fait par dylan le 14/03/2022
+Dernière modifications par dylanPerinetti le 19/03/2022
+*/
+
+
 #include "objet.h"
 #include "joueur.h"
-#include "map.h"
+//#include "map.h"
 
 #define NETTOYER_TERMINAL "cls"				//Si vous etes sur windows dans le CMD remplacer par "cls"  
 
@@ -27,7 +38,7 @@ int main(int argc, char* argv[])
 	            system(NETTOYER_TERMINAL);
 	            Cree4Joueurs(tableau_joueur);
 	            //DebutJeu();					//Initialisations du jeu
-	            GeneMap();
+	            //GeneMap();
 	            break;
 	        case 2:
 	            system(NETTOYER_TERMINAL);
@@ -61,18 +72,18 @@ int main(int argc, char* argv[])
 		ChangerCouleurCite(&tableau_joueur[2].cite_possede[2],tableau_joueur[1].couleur);//TESTE
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
 		ChangerCouleurCite(&tableau_joueur[2].cite_possede[2],tableau_joueur[1].couleur);//TESTE
-	    AfficherCoordonneesCite(tableau_joueur[2].cite_possede[7]);
-	    DeplacerPionDroite(&tableau_joueur[2].pion_possede[7]);     //Déplacer pion a Droite
+	    	AfficherCoordonneesCite(tableau_joueur[2].cite_possede[7]);
+	    	DeplacerPionDroite(&tableau_joueur[2].pion_possede[7]);     //Déplacer pion a Droite
 		DeplacerPionGauche(&tableau_joueur[2].pion_possede[7]);     //Déplacer pion a Gauche
 		DeplacerPionHautDroite(&tableau_joueur[2].pion_possede[7]); //Déplacer pion en Haut a Droite
 		DeplacerPionHautGauche(&tableau_joueur[2].pion_possede[7]); //Déplacer pion en Haut a Gauche
 		DeplacerPionBasDroite(&tableau_joueur[2].pion_possede[7]);	//Déplacer pion en Bas a Droite
 		DeplacerPionBasGauche(&tableau_joueur[2].pion_possede[7]);  //Déplacer pion en Bas a Gauche
-	    AfficherIDCite(tableau_joueur[2].cite_possede[7]);
-	    AfficherIDPion(tableau_joueur[2].pion_possede[7]);
-	    printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
-	    printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
-	    AugmenterNiveauJoueur(&tableau_joueur[3]);				//TESTE 
+	    	AfficherIDCite(tableau_joueur[2].cite_possede[7]);
+	    	AfficherIDPion(tableau_joueur[2].pion_possede[7]);
+	    	printf("\n%d\n",InventaireCite(&tableau_joueur[1]));	//TESTE
+	    	printf("\n%d\n",InventairePions(&tableau_joueur[1]));	//TEST
+	    	AugmenterNiveauJoueur(&tableau_joueur[3]);				//TESTE 
 		AfficherNiveauJoueur(&tableau_joueur[1]);				//TESTE
 		AfficherInventaireJoueur(&tableau_joueur[1]);			//TESTE
 		AfficherCouleurCite(tableau_joueur[2].cite_possede[2]);	//TESTE
