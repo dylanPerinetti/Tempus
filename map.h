715429422ae3749da38c9f118d2eb45134437ec6
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-20 17:27:46 +0100

    Update map.h

--- map.h
+++ map.h
@@ -11,7 +11,7 @@
 int GeneMap();                //Génération de la Map initiale
 char Aleatoire();             //Création du nombre aléatoire entre 0 et 5 qui définit la texture de la tuile lors de la génération
 void Erreur(int error);       //Message d'erreur SDL (UNIQUEMENT)
-void DeplacePionMap(int joueur, int departx, int departy, int arriveex, int arriveey, int nombre_pions_depart, int nombre_pions_arrivee, int carte[10][10], SDL_Renderer* rendu, SDL_Rect *Hex);  //Déplace un pion d'une case a une autre, et recharge la tuile de départ si il n'y a plus de pions dessus
+void DeplacePionMap(int joueur, int departx, int departy, int nombre_pions_depart, int nombre_pions_arrivee, int carte[10][10], SDL_Renderer* rendu, SDL_Rect *Hex);  //Déplace un pion d'une case a une autre, et recharge la tuile de départ si il n'y a plus de pions dessus
 void DecallageHexagoneY(SDL_Rect *Hex, int d);                            //Decalle les coordonées x et y au début de la génération d'une nouvelle rangée d'hexagone
 void GenerationHexagone(char terrain, SDL_Renderer* rendu, SDL_Rect *Hex);          //Crée une image a partir d'un fichier bmp
 void DecallageHexagoneX(SDL_Rect *Hex);				//Decalle la coordonée x après la génération d'un hexagone
