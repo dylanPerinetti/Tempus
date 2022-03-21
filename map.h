16152234622572917ebbc1a6798a54691099862b
dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-21 09:09:47 +0100

    Ajout de Prototype
    void AfficherVersionSDL()
    void InitialiseRect()
    char InitialiseHexagone()

--- map.h
+++ map.h
@@ -1,3 +1,17 @@
+/*
+
+-------------- TEMPUS --------------
+
+Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus" 
+
+Ce fichier contient les prototype des fonction lier a la map dans le jeu Tempus.
+Vous retrouverer par aillieur leur mode d'utilisation.
+Pour mieux comprendre leur fonctionnement rendez-vous sur le fichier "map.c" .
+
+
+Fait par MrTNTX et dylan le 15/03/2022
+Dernière modifications par dylanPerinetti le 21/03/2022
+*/
 #ifndef __MAP_H__
 #define __MAP_H__
 
@@ -8,15 +22,25 @@
 #include <string.h>
 
 
-int GeneMap();                //Génération de la Map initiale
-char Aleatoire();             //Création du nombre aléatoire entre 0 et 5 qui définit la texture de la tuile lors de la génération
-void Erreur(int error);       //Message d'erreur SDL (UNIQUEMENT)
+int GeneMap();					//Génération de la Map initiale
+char CharactereAleatoire();		//Création du nombre aléatoire entre 0 et 5 qui définit la texture de la tuile lors de la génération
+void Erreur(int error);			//Message d'erreur SDL (UNIQUEMENT)
 void DeplacePionMap(int joueur, int departx, int departy, int nombre_pions_depart, int nombre_pions_arrivee, int carte[10][10], SDL_Renderer* rendu, SDL_Rect *Hex);  //Déplace un pion d'une case a une autre, et recharge la tuile de départ si il n'y a plus de pions dessus
-void DecallageHexagoneY(SDL_Rect *Hex, int d);                            //Decalle les coordonées x et y au début de la génération d'une nouvelle rangée d'hexagone
-void GenerationHexagone(char terrain, SDL_Renderer* rendu, SDL_Rect *Hex);          //Crée une image a partir d'un fichier bmp
-void DecallageHexagoneX(SDL_Rect *Hex);				//Decalle la coordonée x après la génération d'un hexagone
-char IntEnChar(int Entier);                         //Transforme un entier en caractère (pour l'utiliser dans la recherche du fichier image)
-void TextureRendu(SDL_Surface *image, SDL_Texture *texture, SDL_Renderer* rendu, SDL_Rect *Hex);      //Applique une texture sur le rendu            
-void NouvelleCite(int joueur, int coordx, int coordy, int taille, SDL_Renderer* rendu, SDL_Rect *Hex);     //Genère une nouvelle cité; Change la couleur d'une cité conquise
+
+
+void GenerationHexagone(char terrain, SDL_Renderer* rendu, SDL_Rect *Hex);			//Crée une image a partir d'un fichier bmp
+
+void InitialiseRect(SDL_Rect* _Rectangle, int _x, int _y, int _largeur,int _hauteur);//Initialise les Rect 
+char InitialiseHexagone(SDL_Renderer* _rendu, SDL_Rect *_Hexagone);	//Crée une ligne Hexagone renvois le
+
+void DecallageHexagoneY(SDL_Rect *Hex, int d);										//Decalle les coordonées x et y au début de la génération d'une nouvelle rangée d'hexagone
+void DecallageHexagoneX(SDL_Rect *Hex);												//Decalle la coordonée x après la génération d'un hexagone
+
+char IntEnChar(int Entier);																			//Transforme un entier en caractère (pour l'utiliser dans la recherche du fichier image)
+void TextureRendu(SDL_Surface *image, SDL_Texture *texture, SDL_Renderer* rendu, SDL_Rect *Hex);	//Applique une texture sur le rendu            
+void NouvelleCite(int joueur, int coordx, int coordy, int taille, SDL_Renderer* rendu, SDL_Rect *Hex);//Genère une nouvelle cité; Change la couleur d'une cité conquise
+
+
+void AfficherVersionSDL();		//Affiche la version de sdl
 
 #endif
