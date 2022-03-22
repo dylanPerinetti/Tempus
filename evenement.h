41fc41b01bdc93c9d99d92e2ae5132b34a6dced2
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-22 10:36:33 +0100

    Update evenement.h

--- evenement.h
+++ evenement.h
@@ -10,18 +10,18 @@ Pour mieux comprendre leur fonctionnement rendez-vous sur le fichier "evenement.
 
 
 Fait par dylan le 21/03/2022
-Dernière modifications par dylanPerinetti le 21/03/2022
+Dernière modifications par dylanPerinetti le 22/03/2022
 */
 #ifndef __EVENEMENT_H__
 #define __EVENEMENT_H__
 
-int DeplacerPionGraphiqueDroite(Pions* _pion);          //Déplacer pion graphiquement a Droite
-int DeplacerPionGraphiqueGauche(Pions* _pion);          //Déplacer pion graphiquement a Gauche
-int DeplacerPionGraphiqueHautDroite(Pions* _pion);      //Déplacer pion en Haut graphiquement a Droite
-int DeplacerPionGraphiqueHautGauche(Pions* _pion);      //Déplacer pion en Haut graphiquement a Gauche
-int DeplacerPionGraphiqueBasDroite(Pions* _pion);       //Déplacer pion en Bas graphiquement a Droite
-int DeplacerPionGraphiqueBasGauche(Pions* _pion);       //Déplacer pion en Bas graphiquement a Gauche
-int PlacerPionGraphiqueSurMap(Pions* _pion,int _x,int _y);//placer Pion graphiquement du stuff du joueur sur la map au coordoner x,y
-int PlacerCiteGraphiqueSurMap(Cite* _cite,int _x,int _y);//plcer Citer du graphiquement stuff du joueur sur la map au coordoner x,y 
+int DeplacerPionGraphiqueDroite(Pions* _pion,int _x,int _y);          //Déplacer pion graphiquement a Droite
+int DeplacerPionGraphiqueGauche(Pions* _pion,int _x,int _y);          //Déplacer pion graphiquement a Gauche
+int DeplacerPionGraphiqueHautDroite(Pions* _pion,int _x,int _y);      //Déplacer pion en Haut graphiquement a Droite
+int DeplacerPionGraphiqueHautGauche(Pions* _pion,int _x,int _y);      //Déplacer pion en Haut graphiquement a Gauche
+int DeplacerPionGraphiqueBasDroite(Pions* _pion,int _x,int _y);       //Déplacer pion en Bas graphiquement a Droite
+int DeplacerPionGraphiqueBasGauche(Pions* _pion,int _x,int _y);       //Déplacer pion en Bas graphiquement a Gauche
+int PlacerPionGraphiqueSurMap(Pions* _pion,int _x,int _y);            //placer Pion graphiquement du stuff du joueur sur la map au coordoner x,y
+int PlacerCiteGraphiqueSurMap(Cite* _cite,int _x,int _y);             //plcer Citer du graphiquement stuff du joueur sur la map au coordoner x,y 
 
 #endif
