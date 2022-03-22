7933778344a841212779e5e02d92fde30d9336c7
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-22 21:32:08 +0100

    CREATION Fonction

--- map.c
+++ map.c
@@ -11,4 +11,34 @@ Fait par MrTNTX le 22/03/2022
 Dernière modifications par dylanPerinetti le 22/03/2022
 
 */
-#include <map.h>
+#include "map.h"
+
+int EcrireTuilesBinaire(Tuile* _tuile)
+{
+	FILE* fic;
+
+	fic = fopen("_DATA/_SAVE.data","wba");
+	if(fic == NULL)
+	{
+		fprintf(stderr, "Erreur ouverture fichier.\n");
+		return 3;
+	}
+	fwrite(_tuile,sizeof(*_tuile),1,fic);
+	fclose(fic);
+	return 0;
+}
+
+int LectureTuilesBinaire(Tuile* _tuile)
+{
+	FILE* fic;
+
+	fic = fopen("_DATA/_SAVE.data", "rb");
+	if(fic == NULL)
+	{
+		fprintf(stderr, "Erreur ouverture fichier.\n");
+		return 3;
+	}
+	fread(_tuile,sizeof(*_tuile),1,fic);
+	fclose(fic);
+	return 0;
+}
