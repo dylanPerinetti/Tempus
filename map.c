37293f2bd073373a50fb90bdd963c4cba8be0681
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-22 13:09:58 +0100

    Create map.c

---
+++ map.c
@@ -0,0 +1,15 @@
+/*
+
+-------------- TEMPUS --------------
+
+Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus" 
+
+Ce fichier contient les fonction liez à la map dans le Jeu Tempus.
+En savoir plus sur leur utilisation dans le ficheier "map.h".
+
+Fait par MrTNTX le 22/03/2022
+Dernière modifications par MrTNTX le 22/03/2022
+
+*/
+#include <objet.h>
+#include <joueur.h>
