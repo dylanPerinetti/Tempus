c5b574cf5b9ecd2fe82f68834c6a20c2b946b893
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-23 21:55:48 +0100

    Update object.c

--- object.c
+++ object.c
@@ -7,8 +7,8 @@ Retrouver le Projet complet sur Git "https://github.com/dylanPerinetti/tempus"
 Ce fichier contient les fonction liez au Objet dans le Jeu Tempus.
 En savoir plus sur leur utilisation dans le ficheier "objet.h".
 
-Fait par dylan le 15/03/2022
-Dernière modifications par dylanPerinetti le 22/03/2022 
+Fait par dylanPerinetti le 15/03/2022
+Dernière modifications par dylanPerinetti le 23/03/2022 
 Pour correctif de beug
 
 */
