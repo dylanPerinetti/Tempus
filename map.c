01d1f9fdc169c09739f466c1993ca9ac01bf94d1
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-24 12:36:57 +0100

    Update map.c

--- map.c
+++ map.c
@@ -28,6 +28,25 @@ Dernière modifications par dylanPerinetti le 24/03/2022
 #define Y_MAX 10
 #define VALEUR_INVENTAIRE 99 // Valeur par deafut de tous se qui a été initialiser /!\ cette dernier dois depasser la plus grande coordonnes de la map soit 10/10
 
+void CreeMapTuile(Tuile _map[10][10])
+{
+	for (unsigned char i = 0; i < Y_MAX; ++i)
+	{
+		for (unsigned char j = 0; j < X_MAX; ++j)
+		{
+			InitialiserCoordonneesTuile(&_map[i][j]);
+			InitialiserTypeTerrainTuile(&_map[i][j]);
+			InitialiserNombresPionsTuile(&_map[i][j]);
+			ChangerCoordonneesTuile(&_map[i][j], i, j);
+			ChangerTypeTerrainTuile(&_map[i][j],1);
+			ChangerNombresPionsTuile(&_map[i][j],1);
+			printf("\nInitialisation de la tuiles x = %u | y = %u \033[36;01mTerminer\033[00m",_map[i][j].coordonnees.X,_map[i][j].coordonnees.Y);
+
+		}
+	}
+}
+
+
 void InitialiserCoordonneesTuile(Tuile* _tuile)
 {
 	_tuile->coordonnees.X = VALEUR_INVENTAIRE;
@@ -35,15 +54,36 @@ void InitialiserCoordonneesTuile(Tuile* _tuile)
 }
 
 
-void InitialiserTypeTerrainTuile(Tuile* _tuile, unsigned char _type_terrain)
+void InitialiserTypeTerrainTuile(Tuile* _tuile)
 {
-	_tuile->type_de_terrain = VALEUR_INVENTAIRE;	
+	_tuile->type_terrain = VALEUR_INVENTAIRE;	
+}
+
+
+void InitialiserNombresPionsTuile(Tuile* _tuile)
+{
+	_tuile->nombre_pion = VALEUR_INVENTAIRE;
+}
+
+
+
+int ChangerNombresPionsTuile(Tuile* _tuile, unsigned char _nombre_pion)
+{
+	_tuile->nombre_pion = _nombre_pion;
+	return 0;
+}
+
+
+int ChangerTypeTerrainTuile(Tuile* _tuile, unsigned char _type_terrain)
+{
+	_tuile->type_terrain = _type_terrain;
+	return 0;	
 }
 
 
 int ChangerCoordonneesTuile(Tuile* _tuile, unsigned char _x, unsigned char _y)
 {
-	if((_tuile.coordonnees.X && _tuile.coordonnees.Y)  == VALEUR_INVENTAIRE)
+	if((_tuile->coordonnees.X == VALEUR_INVENTAIRE)&&(_tuile->coordonnees.Y == VALEUR_INVENTAIRE))
 	{
 		if( _x >= X_MIN && _x <= X_MAX)_tuile->coordonnees.X = _x;
 		else return 41;
@@ -102,12 +142,8 @@ void AfficherErreurEvenement(int _erreur)
 			break;
 		case 22:
 			fprintf(stderr,"\nErreur%d Probleme coordonnees Y",_erreur);
-			break
+			break;
 		default:
 			fprintf(stderr,"\nErreur Inconnue");
 	}
 }
-
-
-
-
