705c987a475a816eca607f563db562962a6cb39d
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-04-08 23:55:46 +0200

    Update deplacement.h

--- src/deplacement.h
+++ src/deplacement.h
@@ -10,7 +10,7 @@ Pour mieux comprendre leur fonctionnement rendez-vous sur le fichier "deplacemen
 
 
 Fait par dylan le 15/03/2022
-Dernière modifications par dylanPerinetti le 06/04/2022 
+Dernière modifications par dylanPerinetti le 08/04/2022 
 Pour correctif de beug
 */
 
