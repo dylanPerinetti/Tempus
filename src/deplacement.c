9c411a87a3831ee4dcb4ca7db32e4cf41dfc0cff
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-04-08 23:55:23 +0200

    Update deplacement.c

--- src/deplacement.c
+++ src/deplacement.c
@@ -8,7 +8,7 @@ Ce fichier contient les fonction liez au deplacement dans le Jeu Tempus.
 En savoir plus sur leur utilisation dans le ficheier "deplacement.h".
 
 Fait par dylan le 15/03/2022
-Dernière modifications par dylanPerinetti le 06/04/2022 
+Dernière modifications par dylanPerinetti le 07/04/2022 
 Pour correctif de beug
 
 */
